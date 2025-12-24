e0fdcac058b9bf4b1fb503af4803a1c913d51a41
cailloufco <caioextreme000@gmail.com>
2025-12-23 22:15:41 -0300

    organização de imagem e funções

--- projeto.js
+++ projeto.js
@@ -1,20 +1,27 @@
 //quantidade inicial para definir se ta desligado ou ligado
 let definidorLigaDesliga = "desliga";
 let lampada = document.getElementById("lampadaApagada");
+let interruptor = document.getElementById("InterruptorDesligado");
+let instagram = document.getElementById("instagram");
 function ligarDesligarLampada() {
   //responsavel por ligar a lampada
+
   if (definidorLigaDesliga == "liga") {
     definidorLigaDesliga = "desliga";
 
     console.log("ligado");
-
-    lampada.src = "assets/lampadaLigada.jpg"
-
+    interruptor.src = "assets/InterruptorLigado.jpg";
+    lampada.src = "assets/lampadaLigada.jpg";
+    instagram.style.display="block";
     //responsavel por desligar a lampada
-  } else  {
+  } else {
     definidorLigaDesliga = "liga";
     console.log("desliga");
+    
+    interruptor.src = "assets/InterruptorDesligado.jpg";
+    
 
     lampada.src = "assets/lampadaApagada.jpg";
-  } 
+    instagram.style.display = "none";
+  }
 }
